395876d35881cb44d0e51eb663e6e9cfa24ef807
Ubuntu <alessandro.erba@ErbaAlessandro.usnb4fs3xwxejg3xfrezipwzac.bx.internal.cloudapp.net>
2018-05-03 17:01:54 +0000

    =fixes

--- experiments.bash
+++ experiments.bash
@@ -3,4 +3,4 @@ python k-fold_cross-validation.py --batch_size=8
 python k-fold_cross-validation.py --batch_size=16
 python k-fold_cross-validation.py --batch_size=24
 python k-fold_cross-validation.py --batch_size=32
-shutdown -t 1
+az vm deallocate -g ErbaAlessandro -n ErbaAlessandro
