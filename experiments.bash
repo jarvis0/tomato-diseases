ae0482382ca47f43a44031b452c3557f9a596101
Ubuntu <alessandro.erba@ErbaAlessandro.usnb4fs3xwxejg3xfrezipwzac.bx.internal.cloudapp.net>
2018-05-07 20:39:09 +0000

    leNet results

--- experiments.bash
+++
@@ -1,5 +0,0 @@
-# to be modified time by time
-python k-fold_cross-validation_multiclass_classification.py --batch_size 32
-python k-fold_cross-validation_multiclass_classification.py --batch_size 24
-python k-fold_cross-validation_multiclass_classification.py --batch_size 16
-az vm deallocate -g ErbaAlessandro -n ErbaAlessandro
