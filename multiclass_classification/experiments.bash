379e40309daa378808e59b1512ae695464318c4b
Ubuntu <alessandro.erba@ErbaAlessandro.usnb4fs3xwxejg3xfrezipwzac.bx.internal.cloudapp.net>
2018-05-09 09:40:39 +0000

    partial tensorboard results

--- multiclass_classification/experiments.bash
+++ multiclass_classification/experiments.bash
@@ -1,3 +1,3 @@
 # to be modified time by time
-python k-fold_cross-validation.py --num_epochs 30 --batch_size 32
+python k-fold_cross-validation.py --num_epochs 100 --batch_size 32
 az vm deallocate -g ErbaAlessandro -n ErbaAlessandro
