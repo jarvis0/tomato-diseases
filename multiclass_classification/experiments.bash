7fc22e39fd9d15fcf534a906f13cf4ee02b150d6
Ubuntu <alessandro.erba@ErbaAlessandro.usnb4fs3xwxejg3xfrezipwzac.bx.internal.cloudapp.net>
2018-05-21 06:44:31 +0000

    segmented first trial

--- multiclass_classification/experiments.bash
+++ multiclass_classification/experiments.bash
@@ -1,5 +1,3 @@
 # to be modified time by time
-python ../b_data_loading.py
-python ../b_data_augmentation.py
-python b_tl_cross_validation.py
+python b_tl_cross-validation.py
 az vm deallocate -g ErbaAlessandro -n ErbaAlessandro
