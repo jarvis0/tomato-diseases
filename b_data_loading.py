bfa4abb046a2565eafa72a98c539820f2c4e57ef
jarvis0 <giuseppe.mascellaro@mail.polimi.it>
2018-05-22 12:57:50 +0200

    segmented with other normalization experiment

--- b_data_loading.py
+++ b_data_loading.py
@@ -15,7 +15,7 @@ if os.path.exists(destination):
 os.mkdir(destination)
 os.mkdir(destination + 'train')
 os.mkdir(destination + 'val')
-#os.mkdir(destination + 'segmented_test')
+os.mkdir(destination + 'segmented_test')
 os.mkdir(destination + 'normal_test')
 
 random.seed(a=1234)
@@ -23,13 +23,13 @@ if not DEBUG:
     for cls in list(classes):    
         os.mkdir(destination + 'train/' + cls)
         os.mkdir(destination + 'val/' + cls)
-        #os.mkdir(destination + 'segmented_test/' + cls)
+        os.mkdir(destination + 'segmented_test/' + cls)
         os.mkdir(destination + 'normal_test/' + cls)
         for image in sorted(os.listdir(source + cls + '/')):
             r = random.randint(0, 100)
             if r <= test_size:
                 # segmented_test
-                #shutil.copyfile(source + cls + '/' + image, destination + 'segmented_test/' + cls + '/' + image)
+                shutil.copyfile(source + cls + '/' + image, destination + 'segmented_test/' + cls + '/' + image)
                 # normal_test
                 shutil.copyfile('original_dataset/' + cls + '/' + image.replace('_final_masked', ''), destination + 'normal_test/' + cls + '/' + image)
             elif r <= test_size + validation_size:
@@ -48,7 +48,7 @@ else:
             r = random.randint(0, 100)
             if r <= test_size:
                 #segmented_test
-                #shutil.copyfile(source + cls + '/' + image, destination + 'segmented_test/' + cls + '/' + image)
+                shutil.copyfile(source + cls + '/' + image, destination + 'segmented_test/' + cls + '/' + image)
                 # normal_test
                 shutil.copyfile('original_dataset/' + cls + '/' + image.replace('_final_masked', ''), destination + 'normal_test/' + cls + '/' + image)
             elif r <= test_size + validation_size:
